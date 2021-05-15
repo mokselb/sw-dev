b6a9606d27eb781e7480c87edbc5b97508b45ca3
mokselb1488 <bat-chat@ukr.net>
2021-05-15 03:31:28 +0300

    Add Laba

---
+++ Laba1Figures/Laba1Figures/Laba1Figures.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+class Rectangle
+{
+public:
+	void set(int a, int b)
+	{
+		width = a;
+		length = b;
+		return;
+	};
+	int Sq(int wid,int len)
+	{
+		int c = 0;
+		c = wid * len;
+		return c;
+	};
+	int Pr(int wid, int len)
+	{
+		int c = 0;
+		c = 2 * (wid + len);
+		return c;
+	};
+private:
+	int width = 0;
+	int length = 0;
+};
+class Square : public Rectangle
+{
+
+};
+int main()
+{
+	Rectangle rec;
+	Square squ;
+	cout << "Enter rec w and l : ";
+	int w, l;
+	cin >> w >> l;
+	cout << endl;
+	cout << rec.Pr(w, l) << endl;
+	cout << rec.Sq(w, l) << endl;
+	cout << "Enter squ side :";
+	int a;
+	cin >> a;
+	cout << endl;
+	cout << squ.Pr(a,a)<< endl;
+	cout << squ.Sq(a,a)<< endl;
+	return 0;
+}
+
---
+++ Laba1Figures/UnitTest2/UnitTest2.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba1Figures/Laba1Figures.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Rectangle rectangle;
+			Assert::AreEqual(rectangle.Pr(6, 8), 28);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			Square squar;
+			Assert::AreEqual(squar.Pr(5,5), 20);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Rectangle rectangle;
+			Assert::AreEqual(rectangle.Sq(10, 0), 0);
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			Square squar;
+			Assert::AreEqual(squar.Sq(5, 5), 25);
+		}
+	};
+}
